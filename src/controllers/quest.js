f9467aa1d3de2f8c0a44b8f01e7e7b87d39aab19
ale.chaito <alechaito@gmail.com>
2021-12-30 19:45:20 -0300

    using throw

--- src/controllers/quest.js
+++ src/controllers/quest.js
@@ -1,14 +1,14 @@
-const {InventoryMonsters, Monsters} = require("../models/monster");
-const {randomTokenReward, getExpByRarity, giveTokenReward} = require("./reward");
+const { InventoryMonsters, Monsters } = require("../models/monster");
+const { randomTokenReward, getExpByRarity, giveTokenReward } = require("./reward");
 const Helper = require("./helper");
 const Users = require("../models/user");
 
 var wallet = "0x6C3CF1365a872915D8F6ab03C89326F28C8a146c";
 
 var quests = [
-    {lvl_min: 0, lvl_max: 5, gas: 300, reward_min: 3, reward_max: 10, success: 81},
-    {lvl_min: 6, lvl_max: 10, gas: 300, reward_min: 6, reward_max: 15, success: 81},
-    {lvl_min: 11, lvl_max: 20, gas: 300, reward_min: 9, reward_max: 20, success: 81},
+    { lvl_min: 0, lvl_max: 5, gas: 300, reward_min: 3, reward_max: 10, success: 81 },
+    { lvl_min: 6, lvl_max: 10, gas: 300, reward_min: 6, reward_max: 15, success: 81 },
+    { lvl_min: 11, lvl_max: 20, gas: 300, reward_min: 9, reward_max: 20, success: 81 },
 ];
 
 const getQuestInfos = async (idx_quest, level) => {
@@ -38,32 +38,28 @@ const isSuccess = async (percentage) => {
 
 const start = async (req, res) => {
     try {
-        let {monster_id, quest_id} = req.body;
+        let { monster_id, quest_id } = req.body;
         let myMonster = await InventoryMonsters.findOne({
-            where: {id: monster_id},
+            where: { id: monster_id },
         });
 
-        let user = await Users.findOne({where: {id: myMonster.id_user, wallet: wallet}});
+        let user = await Users.findOne({ where: { id: myMonster.id_user, wallet: wallet } });
         if (!user) {
-            console.log("Player is not owner of this monster.");
-            return res.redirect("/monster/view/all");
+            throw new Error("Player is not owner of this monster.");
         }
 
         // check if monster of user is able to start this dungeon based on level
         let questInfo = await getQuestInfos(quest_id, myMonster.level);
         if (questInfo == false) {
-            console.log("The monster does not have the required minimum level.");
-            return res.redirect("/monster/view/all");
+            throw new Error("The monster does not have the required minimum level.");
         }
 
         if (myMonster.feed == 0) {
-            console.log("Monster are hungry and connot start a dungeon, please feed it.");
-            return res.redirect("/monster/view/all");
+            throw new Error("Monster are hungry and connot start a dungeon, please feed it.");
         }
 
         if (!(await isSuccess(questInfo.success))) {
-            console.log("You have failed to complet the quest.");
-            return res.redirect("/monster/view/all");
+            throw new Error("You have failed to complet the quest.");
         }
 
         // All things are checked and if we are here we can give rewards
@@ -72,14 +68,14 @@ const start = async (req, res) => {
         let tokenReward = await randomTokenReward(questInfo.reward_min, questInfo.reward_max);
         console.log("token reward", tokenReward);
         // getting exp reward by rarity and table
-        let monster = await Monsters.findOne({where: {id: myMonster.id_monster}});
+        let monster = await Monsters.findOne({ where: { id: myMonster.id_monster } });
         let expReward = await getExpByRarity(monster.rarity);
         console.log("exp reward", expReward);
         // save on database rewards gained
         // setting tokens to balance
         await giveTokenReward(myMonster, tokenReward, user.wallet);
         // setting new monster exp
-        await myMonster.update({exp: parseFloat(myMonster.exp) + parseFloat(expReward)});
+        await myMonster.update({ exp: parseFloat(myMonster.exp) + parseFloat(expReward) });
         return res.redirect("/monster/view/all");
     } catch (error) {
         console.log(error);
@@ -87,4 +83,4 @@ const start = async (req, res) => {
     }
 };
 
-module.exports = {start};
+module.exports = { start };
